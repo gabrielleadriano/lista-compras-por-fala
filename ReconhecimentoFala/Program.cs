9a7a6b287b6877c4230eb31a20609e8343ff828d
Gabrielle Adriano <gabrielle.a.adriano@gmail.com>
2022-10-05 20:00:13 -0300

    criar lista de compras e contar piada

--- ReconhecimentoFala/Program.cs
+++ ReconhecimentoFala/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
@@ -8,28 +7,74 @@ class Program
 {
     static string YourSubscriptionKey = "YourSubscriptionKey";
     static string YourServiceRegion = "YourServiceRegion";
+    static SpeechRecognizer speechRecognizer;
 
-    static void OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
+    async static Task OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
     {
         switch (speechRecognitionResult.Reason)
         {
             case ResultReason.RecognizedSpeech:
-                Console.WriteLine($"RECOGNIZED: Text={speechRecognitionResult.Text}");
+                if (speechRecognitionResult.Text.ToLower().Contains("lista de compras"))
+                    await buildShoppingList();
+                else if (speechRecognitionResult.Text.ToLower().Contains("piada"))
+                    await tellJoke();
                 break;
             case ResultReason.NoMatch:
-                Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                Console.WriteLine("NÃO FOI POSSÍVEL RECONHECER A FALA");
                 break;
             case ResultReason.Canceled:
                 var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
-                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+                Console.WriteLine($"CANCELADO: Motivo={cancellation.Reason}");
 
                 if (cancellation.Reason == CancellationReason.Error)
                 {
-                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
-                    Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
-                    Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                    Console.WriteLine($"CANCELADO: ErrorCode={cancellation.ErrorCode}");
+                    Console.WriteLine($"CANCELADO: ErrorDetails={cancellation.ErrorDetails}");
+                    Console.WriteLine($"CANCELADO: Did you set the speech resource key and region values?");
                 }
                 break;
+        }
+    }
+
+    async static Task buildShoppingList()
+    {
+        bool isComplete = false;
+
+        while (!isComplete)
+        {
+            Console.WriteLine("INFORME OS ITENS DA LISTA DE COMPRAS");
+            var listRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+
+            switch (listRecognitionResult.Reason)
+            {
+                case ResultReason.RecognizedSpeech:
+                    var speechConfig = SpeechConfig.FromSubscription(YourSubscriptionKey, YourServiceRegion);
+                    speechConfig.SpeechSynthesisVoiceName = "pt-BR-AntonioNeural";
+
+                    using (var speechSynthesizer = new SpeechSynthesizer(speechConfig))
+                    {
+                        string text = $"SUA LISTA É: {listRecognitionResult.Text}";
+                        await speechSynthesizer.SpeakTextAsync(text);
+                    }
+
+                    isComplete = true;
+                    break;
+                case ResultReason.NoMatch:
+                    Console.WriteLine("NÃO FOI POSSÍVEL RECONHECER A FALA, TENTE NOVAMENTE");
+                    break;
+            }
+        }
+    }
+
+    async static Task tellJoke()
+    {
+        var speechConfig = SpeechConfig.FromSubscription(YourSubscriptionKey, YourServiceRegion);
+        speechConfig.SpeechSynthesisVoiceName = "pt-BR-AntonioNeural";
+
+        using (var speechSynthesizer = new SpeechSynthesizer(speechConfig))
+        {
+            string text = $"Porque o jacaré levou uma bronca?...Porque ele RÉPTIL de ano";
+            await speechSynthesizer.SpeakTextAsync(text);
         }
     }
 
@@ -39,10 +84,9 @@ class Program
         speechConfig.SpeechRecognitionLanguage = "pt-BR";
 
         using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
-        using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
-
-        Console.WriteLine("Speak into your microphone.");
+        speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+        Console.WriteLine("INFORME O COMANDO");
         var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
-        OutputSpeechRecognitionResult(speechRecognitionResult);
+        await OutputSpeechRecognitionResult(speechRecognitionResult);
     }
 }
